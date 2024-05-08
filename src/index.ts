0462e6aea078f6f2acd7c4f69008f67c106e54a8
PVA12 <pvabratenota@gmail.com>
2024-05-08 12:44:32 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { UserModel } from "./models/user.model";
 import { config } from "dotenv";
-import { Client, Collection, GatewayIntentBits, ActivityType, GuildMember } from "discord.js"; // Добавлен импорт ActivityType
-import EventHandlerService from "./events/system/collectors/event.collector";
 import {
-  Button,
-  SlashCommand,
-  CustomId,
-} from "./base.types";
+  Client,
+  Collection,
+  GatewayIntentBits,
+  ActivityType,
+
+} from "discord.js"; // Добавлен импорт ActivityType
+import EventHandlerService from "./events/system/collectors/event.collector";
+import { Button, SlashCommand, CustomId } from "./base.types";
 import mongoose from "mongoose";
 config();
 
@@ -15,8 +17,7 @@ declare module "discord.js" {
     commands?: Collection<string, SlashCommand>;
     buttons?: Collection<CustomId, Button>;
   }
-} 
-
+}
 
 const intents = [
   GatewayIntentBits.Guilds,
@@ -27,47 +28,47 @@ const intents = [
   GatewayIntentBits.GuildVoiceStates,
 ]; // обычный массив интентов
 
+const client: Client = new Client({ intents: intents });
 
-const client: Client = new Client({ intents: intents }); 
-
-client.commands = new Collection<string, SlashCommand>(); 
-client.buttons = new Collection<CustomId, Button>(); 
-new EventHandlerService(client); 
+client.commands = new Collection<string, SlashCommand>();
+client.buttons = new Collection<CustomId, Button>();
+new EventHandlerService(client);
 
 client.once("ready", async () => {
-
-
   console.log("Bot is ready!");
-  client.user?.setActivity('.help', { type: ActivityType.Listening }); 
-await mongoose.connect(
-    process.env.MIGRATE_MONGO_URI || "mongodb://127.0.0.1:27017/",
+  client.user?.setActivity(".help", { type: ActivityType.Listening });
+  await mongoose.connect(
+    process.env.MIGRATE_MONGO_URI || "mongodb://127.0.0.1:27017/TEEST",
     {
-      autoCreate: false, 
-      bufferCommands: true
+      autoCreate: false,
+      bufferCommands: true,
     }
   );
-  const guilds = await client.guilds.fetch();
-  guilds.map(async (guild: any): Promise<any> => {
-  const members = await guild.members;
-  if (members.length < 1) return;
-  members.map(async (member: GuildMember): Promise<any> => {
-    if (member.user.bot) {
+  try {
+    const guilds = await client.guilds.cache;
+    guilds.forEach(async (guild: any) => {
+      const members = await guild.members.fetch(); 
+      if (members.size < 1) return;
+      members.forEach(async (member: any) => {
+        if (member.user.bot) {
+          return null;
+        }
 
-      return null;
-    }
-
-    const userFromDb = await UserModel.findOne({
-      guildId: guild.id,
-      userId: member.user.id,
-    });
-    if (!userFromDb) {
-      await UserModel.create({
-        userId: member.user.id,
-        guildId: guild.id,
+        const userFromDb = await UserModel.findOne({
+          guildId: guild.id,
+          userId: member.user.id,
+        });
+        if (!userFromDb) {
+          await UserModel.create({
+            userId: member.user.id,
+            guildId: guild.id,
+          });
+        }
       });
-    }
-  });
-});
+    });
+  } catch (error) {
+    console.error("Error fetching guilds:", error);
+  };
 });
 
 client.login(process.env.TOKEN);
